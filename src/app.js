d7cd0eb71219153d9526aee97c7a69aa1738b2b4
DMV-25 <martinezvasquezdany@gmail.com>
2025-11-13 02:39:21 +0000

    proyecto modificado

--- src/app.js
+++ src/app.js
@@ -10,11 +10,14 @@ window.onload = function () {
   let pronoun = ['the', 'our'];
   let adj = ['great', 'big'];
   let noun = ['jogger', 'racoon'];
+  let extensions =['.com', '.net','.io','.tech']
 
   for (let a = 0; a < pronoun.length; a++) {
     for (let b = 0; b < adj.length; b++) {
-      for (let c = 0; c < noun.length; c++) {
-        console.log(pronoun[a] + adj[b] + noun[c] + ".com")
+      for (let c = 0; c < noun.length; c++) 
+        for (let d = 0; d <extensions.length; d++)
+        {
+        console.log(pronoun[a] + " " + adj[b] + " " + noun[c] + " " + extensions[d])
       }
     }
   }
